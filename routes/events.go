29a65eaf3a2a8f7eda46517b653afbfa2b8e4872
Tauan Camargo <lukyhenson@gmail.com>
2024-05-27 17:32:55 -0300

    feat: add events route handler

---
+++ routes/events.go
@@ -0,0 +1,56 @@
+package routes
+
+import (
+	"net/http"
+	"strconv"
+
+	"example.com/rest-api-go/models"
+	"github.com/gin-gonic/gin"
+)
+
+func getEvents(context *gin.Context) {
+	events, err := models.GetAllEvents()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not get events!"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{
+		"events": events,
+	})
+}
+
+func getEvent(context *gin.Context) {
+	id, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Invalid ID!"})
+		return
+	}
+
+	event, err := models.GetEventByID(id)
+
+	if err != nil {
+		context.JSON(http.StatusNotFound, gin.H{"message": "Event not found!"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{
+		"event": event,
+	})
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not save event!"})
+		return
+	}
+
+	event.UserID = 1
+	event.Save()
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created!"})
+}
