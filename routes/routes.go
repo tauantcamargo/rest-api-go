6804448a6ec3c503ffe4cfeae566d76d62fad051
Tauan Camargo <lukyhenson@gmail.com>
2024-05-27 20:26:50 -0300

    feat: create registration route

--- routes/routes.go
+++ routes/routes.go
@@ -12,8 +12,10 @@ func RegisterRoutes(server *gin.Engine) {
 		authenticated.POST("/events", middlewares.Authenticate, createEvent)
 		authenticated.PUT("/events/:id", updateEvent)
 		authenticated.DELETE("/events/:id", deleteEvent)
+		authenticated.POST("/events/:id/register", registerForEvent)
+		authenticated.DELETE("/events/:id/register", cancelRegistration)
 	}
-	
+
 	server.GET("/events", getEvents)
 	server.GET("/events/:id", getEvent)
 
