8a23cec92ed2421bc4f78805da2f26118674d802
Tauan Camargo <lukyhenson@gmail.com>
2024-05-27 17:24:56 -0300

    feat: add fetch by id to server route

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"example.com/rest-api-go/db"
 	"example.com/rest-api-go/models"
@@ -13,6 +14,7 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.GET("/events/:id", getEvent)
 	server.POST("/events", createEvent)
 
 	server.Run(":8080")
@@ -28,6 +30,26 @@ func getEvents(context *gin.Context) {
 
 	context.JSON(http.StatusOK, gin.H{
 		"events": events,
+	})
+}
+
+func getEvent(context *gin.Context) {
+	id, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Invalid ID!"})
+		return
+	}
+
+	event, err := models.GetEventByID(id)
+
+	if err != nil {
+		context.JSON(http.StatusNotFound, gin.H{"message": "Event not found!"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{
+		"event": event,
 	})
 }
 
