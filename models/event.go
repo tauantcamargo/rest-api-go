2618d21bfa6f4af252cb297277718d38cd5dcda7
Tauan Camargo <lukyhenson@gmail.com>
2024-05-27 17:43:54 -0300

    feat: add update model handler

--- models/event.go
+++ models/event.go
@@ -40,6 +40,26 @@ func (e Event) Save() error {
 	return err
 }
 
+func (e Event) Update() error {
+	query := `
+		UPDATE events 
+		SET name = ?, description = ?, location = ?, dateTime = ? 
+		WHERE id = ?
+	`
+
+	stmt, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.ID)
+
+	return err
+}
+
 func GetAllEvents() ([]Event, error) {
 	query := `
 		SELECT * FROM events
