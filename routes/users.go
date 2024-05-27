d273db0b3be001afe3d9f51f9e2103d2ce9c9820
Tauan Camargo <lukyhenson@gmail.com>
2024-05-27 18:35:28 -0300

    feat: add login handler to user route

--- routes/users.go
+++ routes/users.go
@@ -26,3 +26,23 @@ func signup(context *gin.Context) {
 
 	context.JSON(http.StatusCreated, gin.H{"message": "User created!"})
 }
+
+func login(context *gin.Context) {
+	var user models.User
+
+	err := context.ShouldBindJSON(&user)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Invalid request!"})
+		return
+	}
+
+	err = user.ValidateCredentials()
+
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "User logged in!"})
+}
