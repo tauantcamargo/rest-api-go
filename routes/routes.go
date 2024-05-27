73cfbb464d90e245b938bfa66c6f29e56ff5577d
Tauan Camargo <lukyhenson@gmail.com>
2024-05-27 20:08:03 -0300

    feat: add group authenticated routes

--- routes/routes.go
+++ routes/routes.go
@@ -1,13 +1,21 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"example.com/rest-api-go/middlewares"
+	"github.com/gin-gonic/gin"
+)
 
 func RegisterRoutes(server *gin.Engine) {
+	authenticated := server.Group("/")
+	{
+		authenticated.Use(middlewares.Authenticate)
+		authenticated.POST("/events", middlewares.Authenticate, createEvent)
+		authenticated.PUT("/events/:id", updateEvent)
+		authenticated.DELETE("/events/:id", deleteEvent)
+	}
+	
 	server.GET("/events", getEvents)
 	server.GET("/events/:id", getEvent)
-	server.POST("/events", createEvent)
-	server.PUT("/events/:id", updateEvent)
-	server.DELETE("/events/:id", deleteEvent)
 
 	server.POST("/signup", signup)
 	server.POST("/login", login)
