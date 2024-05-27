dfa368ef3bdbd405cc3022e4630e73166a785fd8
Tauan Camargo <lukyhenson@gmail.com>
2024-05-27 17:10:57 -0300

    refactor: update api to save and get from sqlite

--- models/event.go
+++ models/event.go
@@ -1,9 +1,13 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"example.com/rest-api-go/db"
+)
 
 type Event struct {
-	ID          string    `json:"id"`
+	ID          int64     `json:"id"`
 	Name        string    `json:"name" binding:"required"`
 	Description string    `json:"description" binding:"required"`
 	Location    string    `json:"location" binding:"required"`
@@ -13,12 +17,52 @@ type Event struct {
 
 var events = []Event{}
 
-func (e Event) Save() {
-	// TODO: Add it to database
+func (e Event) Save() error {
+	query := `
+	INSERT INTO events (name, description, location, dateTime, user_id) 
+	VALUES (?, ?, ?, ?, ?)
+	`
 
-	events = append(events, e)
+	stmt, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+
+	e.ID = id
+	return err
 }
 
-func GetAllEvents() []Event {
-	return events
+func GetAllEvents() ([]Event, error) {
+	query := `
+		SELECT * FROM events
+	`
+
+	rows, err := db.DB.Query(query)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var events []Event
+
+	for rows.Next() {
+		var event Event
+		rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+
+		events = append(events, event)
+	}
+
+	return events, nil
 }
