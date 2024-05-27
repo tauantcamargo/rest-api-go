c90053a205f08ff0a3e7584cb3c60b9a61e35b3f
Tauan Camargo <lukyhenson@gmail.com>
2024-05-27 20:08:17 -0300

    feat: update jwt logic to return userId

--- utils/jwt.go
+++ utils/jwt.go
@@ -19,7 +19,7 @@ func GenerateToken(email string, userId int64) (string, error) {
 	return token.SignedString([]byte(secretKey))
 }
 
-func VerifyToken(token string) error {
+func VerifyToken(token string) (int64, error) {
 	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
 		_, ok := token.Method.(*jwt.SigningMethodHMAC)
 
@@ -31,22 +31,23 @@ func VerifyToken(token string) error {
 	})
 
 	if err != nil {
-		return err
+		return 0, err
 	}
 
 	tokenIsValid := parsedToken.Valid
 
 	if !tokenIsValid {
-		return errors.New("Token is invalid!")
+		return 0, errors.New("Token is invalid!")
 	}
 
-	// claims, ok := parsedToken.Claims.(jwt.MapClaims)
+	claims, ok := parsedToken.Claims.(jwt.MapClaims)
 
-	// if !ok {
-	//   return errors.New("There was an error!")
-	// }
+	if !ok {
+		return 0, errors.New("There was an error!")
+	}
 
 	// email := claims["email"].(string)
-	// userId := claims["userId"].(int64)
-	return nil
+	userId := int64(claims["userId"].(float64))
+
+	return userId, nil
 }
