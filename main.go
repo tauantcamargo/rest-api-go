8618ee17a4ced34e166234b8efb920217faef66e
Tauan Camargo <lukyhenson@gmail.com>
2024-05-27 17:11:11 -0300

    feat: update main package

--- main.go
+++ main.go
@@ -19,7 +19,13 @@ func main() {
 }
 
 func getEvents(context *gin.Context) {
-	events := models.GetAllEvents()
+	events, err := models.GetAllEvents()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not get events!"})
+		return
+	}
+
 	context.JSON(http.StatusOK, gin.H{
 		"events": events,
 	})
@@ -34,8 +40,7 @@ func createEvent(context *gin.Context) {
 		return
 	}
 
-	event.ID = "1"
 	event.UserID = 1
 	event.Save()
-	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created!"})
 }
