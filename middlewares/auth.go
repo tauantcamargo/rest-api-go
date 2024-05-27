d0f13b5df6547dc63a1a57524790cb30874b9931
Tauan Camargo <lukyhenson@gmail.com>
2024-05-27 20:06:57 -0300

    feat: add middleware to check authorization

---
+++ middlewares/auth.go
@@ -0,0 +1,28 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"example.com/rest-api-go/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func Authenticate(context *gin.Context) {
+	token := context.Request.Header.Get("Authorization")
+
+	if token == "" {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Unauthorized!"})
+		return
+	}
+
+	userId, err := utils.VerifyToken(token)
+
+	if err != nil {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Unauthorized!"})
+		return
+	}
+
+  context.Set("userId", userId)
+
+  context.Next()
+}
